c0c1b43effe522e9441397c8824daf686a12134c
AzureAD\Peter-TamasBacso <peter.bacso@principal33.com>
2021-03-25 13:11:20 +0200

    replaced js with jquery

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -1,5 +1,12 @@
-document.getElementById("add").onclick = function () {
-    var node = document.createElement("li");
-    node.innerText = newcomer.value;
-    list.appendChild(node);
-};
+// This JS file now uses jQuery. Pls see here: https://jquery.com/
+$(document).ready(function () {
+    // see https://api.jquery.com/click/
+    $("#add").click(function () {
+        var newcomerName = $("#newcomer").val();
+
+        // Remember string interpolation
+        $("#list").append(`<li>${newcomerName}</li>`);
+
+        $("#newcomer").val("");
+    })
+});

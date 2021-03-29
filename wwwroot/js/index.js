d3a029ff98c5db44ea8670d1a81396b25551a463
AzureAD\Peter-TamasBacso <peter.bacso@principal33.com>
2021-03-29 12:03:30 +0300

    display tomorrow weather

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -73,4 +73,37 @@ $(document).ready(function () {
         });
 
     })
+
+    // populate weather fields
+    $.ajax({
+        url: `/WeatherForecast`,
+        success: function (data) {
+            let tomorrow = data[0];
+            let tomorrowDate = formatDate(tomorrow.date)
+
+            $('#date').text(tomorrowDate);
+            $('#temperature').text(tomorrow.temperatureC + ' C');
+            $('#summary').text(tomorrow.summary);
+        },
+        error: function (data) {
+            alert(`failed to load data`);
+        },
+    });
+
+    function formatDate(jsonDate) {
+
+        function join(t, a, s) {
+            function format(m) {
+                let f = new Intl.DateTimeFormat('en', m);
+                return f.format(t);
+            }
+            return a.map(format).join(s);
+        }
+
+        let date = new Date(jsonDate);
+        let a = [{ day: 'numeric' }, { month: 'short' }, { year: 'numeric' }];
+        let s = join(date, a, '-');
+        return s;
+    }
+
 });

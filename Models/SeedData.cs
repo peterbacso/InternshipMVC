6f0e10c05b6c8be8df227f23eb83da5842ec5aa1
AzureAD\Peter-TamasBacso <peter.bacso@principal33.com>
2021-04-19 14:17:57 +0300

    separate readd seed data if it is missing

--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -8,35 +8,39 @@ namespace InternshipMvc.Data
 {
     public static class SeedData
     {
+        private static Location defaultLocation;
+
         public static void Initialize(InternDbContext context)
         {
             context.Database.Migrate();
 
-            if (context.Interns.Any())
+
+            if (!context.Locations.Any())
             {
-                return;   // DB has been seeded
+
+                var locations = new Location[]
+                {
+                    defaultLocation = new Location { Name = "Kyiv", NativeName = "Київ", Longitude = 30.5167, Latitude = 50.4333, },
+                    new Location { Name = "Brasov", NativeName = "Braşov", Longitude = 25.3333, Latitude = 45.75, },
+                };
+
+                context.Locations.AddRange(locations);
+                context.SaveChanges();
             }
 
-            Location defaultLocation;
-
-            var locations = new Location[]
+            if (!context.Interns.Any())
             {
-                defaultLocation = new Location { Name = "Kyiv", NativeName = "Київ", Longitude = 30.5167, Latitude = 50.4333, },
-                new Location { Name = "Brasov", NativeName = "Braşov", Longitude = 25.3333, Latitude = 45.75, },
-            };
 
-            context.Locations.AddRange(locations);
+                var interns = new Intern[]
+                {
+                    new Intern { Name = "Borys", RegistrationDateTime = DateTime.Parse("2021-04-01"), Location = defaultLocation },
+                    new Intern { Name = "Liova", RegistrationDateTime = DateTime.Parse("2021-04-01"), Location = defaultLocation },
+                    new Intern { Name = "Orest", RegistrationDateTime = DateTime.Parse("2021-03-31"), Location = defaultLocation },
+                };
 
-            var interns = new Intern[]
-            {
-                new Intern { Name = "Borys", RegistrationDateTime = DateTime.Parse("2021-04-01"), Location = defaultLocation },
-                new Intern { Name = "Liova", RegistrationDateTime = DateTime.Parse("2021-04-01"), Location = defaultLocation },
-                new Intern { Name = "Orest", RegistrationDateTime = DateTime.Parse("2021-03-31"), Location = defaultLocation },
-            };
-
-            context.Interns.AddRange(interns);
-
-            context.SaveChanges();
+                context.Interns.AddRange(interns);
+                context.SaveChanges();
+            }
         }
     }
 }
